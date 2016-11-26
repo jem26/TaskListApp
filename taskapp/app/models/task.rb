c009f5bea919c36fc757acd649b0d785d0089153
Jamie Maher <jamiemaher@Jamies-MBP.att.net>
2016-11-26 16:23:51 -0600

    validations, scopes, migrations

--- taskapp/app/models/task.rb
+++ taskapp/app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ActiveRecord::Base
   belongs_to :TaskList
+  scope :completed, -> {where(is_completed: true).reverse}
+  scope :pending, -> {where(is_completed: false).order(:created_at)}
+  validates :title, :due_date, :priority, :TaskList_id, presence: true
+  validates :priority, inclusion: { in: 1..10, message: "must be in range 1-10" }
 end
--- taskapp/db/schema.rb
+++ taskapp/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102013051) do
+ActiveRecord::Schema.define(version: 20161126194936) do
 
   create_table "task_lists", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "is_private"
   end
 
   create_table "tasks", force: :cascade do |t|
