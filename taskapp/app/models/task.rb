aafe501feeda23122d66a8d62eb482473f4a59f5
Jonathan Arp <jarp@nd.edu>
2016-12-04 21:22:49 -0500

    notes

--- taskapp/app/models/task.rb
+++ taskapp/app/models/task.rb
@@ -1,8 +1,11 @@
 class Task < ActiveRecord::Base
   belongs_to :TaskList
   belongs_to :User
-  scope :completed, -> {where(is_completed: true).reverse}
+
+  # jarp: i'd use order for this one as well
+  scope :completed, -> {where(is_completed: true).order(created_at: :desc)}
   scope :pending, -> {where(is_completed: false).order(:created_at)}
+
   validates :title, :due_date, :priority, :TaskList_id, presence: true
   validates :priority, inclusion: { in: 1..10, message: "must be in range 1-10" }
 end
