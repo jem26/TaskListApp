bb574f6b21e96bf11de13ad4dc3a86801a654fac
Jonathan Arp <jarp@nd.edu>
2016-12-16 15:43:07 -0500

    notes

--- taskapp/app/controllers/protected_controller.rb
+++ taskapp/app/controllers/protected_controller.rb
@@ -1,14 +1,15 @@
 class ProtectedController < ApplicationController
 	before_action :require_login
 	before_action :set_active_user
+
 	def require_login
 		unless session[:user_id]
 			flash[:message] = "You are not allowed here"
 			redirect_to login_path
 		end
 	end
-
+	# nice!
 	def set_active_user
-    	@active_user = User.find(session[:user_id])
-  	end
+    @active_user = User.find(session[:user_id])
+  end
 end
